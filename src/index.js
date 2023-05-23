20728d61f045b6966f08e6315d3edd69093c9123
unknown <marhoul2200@gmail.com>
2023-05-23 22:06:48 +0100

    u

--- src/index.js
+++ src/index.js
@@ -5,18 +5,13 @@ import "./index.scss";
 import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import cartReducer from './Redux/Slices/cartSlice.js'
 import authReducer from './Redux/Slices/authSlice.js'
-import { productsAPI } from './Redux/API/ProductsAPI'
 
 
 export const store = configureStore({
   reducer: {
-    [productsAPI.reducerPath]: productsAPI.reducer,
-    cart: cartReducer,
     auth: authReducer
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(productsAPI.middleware)
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
